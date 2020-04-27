40a5ce499bc7b16d5a15e4d6e286d571c275aeda
EC2 Default User <ec2-user@ip-172-31-45-30.eu-west-1.compute.internal>
2020-04-27 23:09:00 +0000

    changes

--- route53.tf
+++ route53.tf
@@ -6,7 +6,7 @@ resource "aws_route53_zone" "www" {
 
 resource "aws_route53_record" "www" {
    zone_id = "${aws_route53_zone.www.zone_id}"
-    name    = "azymberdi.com"
+    name    = "www.azymberdi.com"
    type    = "A"
   ttl     = "300"
   records = ["${aws_instance.instance.public_ip}"]
