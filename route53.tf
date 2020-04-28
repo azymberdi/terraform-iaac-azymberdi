0e6bf0aa371c1afb2000c7f54c6794e761b8c24a
azymberdi <azymberdi@gmail.com>
2020-04-28 02:06:17 +0000

    last changes

--- route53.tf
+++ route53.tf
@@ -1,11 +1,11 @@
 
-resource "aws_route53_zone" "www" {
-  name = "azymberdi.com"
-}
+# resource "aws_route53_zone" "www" {
+#   name = "azymberdi.com"
+# }
 
 
 resource "aws_route53_record" "www" {
-   zone_id = "${aws_route53_zone.www.zone_id}"
+   zone_id = "Z0087054W7TKM54TM8MW"
     name    = "www.azymberdi.com"
    type    = "A"
   ttl     = "300"
