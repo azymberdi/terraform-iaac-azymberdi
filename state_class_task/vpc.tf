f4d2130b04413d18a9960c066f98430861dbafc8
azymberdi <azymberdi@gmail.com>
2020-04-30 08:04:26 +0000

    started working on task

--- state_class_task/vpc.tf
+++ state_class_task/vpc.tf
@@ -1,5 +1,12 @@
 resource "aws_vpc" "main" {
-  cidr_block = "${var.vpc_cidr}"
+  # cidr_block = "${var.vpc_cidr}"
+  cidr_block = "10.0.0.0/16"
+  tags =  {
+           Environment = "Dev"
+           Department = "IT"
+           Team = "Infrastructure"
+           Created_by = "Azymberdi Gutdanov"
+  }
 }
 
 # resource "aws_subnet" "main" {
@@ -51,7 +58,7 @@ resource "aws_internet_gateway" "gw" {
 # }
 
 resource "aws_eip" "lb" {
-  instance = "${aws_instance.wordpress.id}"
+  instance = "${aws_instance.instance.id}"
   vpc      = true
 }
 
@@ -77,3 +84,53 @@ resource "aws_route_table_association" "b3" {
   subnet_id      = "${aws_subnet.public3.id}"
   route_table_id = "${aws_route_table.r.id}"
 }
+
+resource "aws_key_pair" "us-east-1-key" {
+  key_name   = "Task3_keypair"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_instance" "instance" {
+  ami           =   "ami-0323c3dd2da7fb37d"
+  instance_type =   "t2.micro"
+  key_name      =   "${aws_key_pair.us-east-1-key.key_name}"
+  security_groups = ["${aws_security_group.allow_vpc.name}"]
+  user_data     =    "${file("httpd.sh")}"
+  tags = {
+    Name = "Website"
+  }
+}
+
+resource "aws_security_group" "allow_vpc" {
+  name        = "allow_tls_azymberdi"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
--- state_class_task/website.tf
+++ state_class_task/website.tf
@@ -1,72 +1,72 @@
-provider "aws" {
-  region = "us-west-2"
-}
+# provider "aws" {
+#   region = "us-west-2"
+# }
 
-resource "aws_key_pair" "us-west-2-key" {
-  key_name   = "Bastion2"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
+# resource "aws_key_pair" "us-west-2-key" {
+#   key_name   = "Bastion2"
+#   public_key = "${file("~/.ssh/id_rsa.pub")}"
+# }
 
-resource "aws_instance" "instance" {
-  ami           =   "ami-0d6621c01e8c2de2c"
-  instance_type =   "t2.micro"
-  key_name      =   "${aws_key_pair.us-west-2-key.key_name}"
-  security_groups = ["${aws_security_group.allow.name}"]
-  user_data     =    "${file("httpd.sh")}"
-  tags = {
-    Name = "Website"
-  }
-}
+# resource "aws_instance" "instance" {
+#   ami           =   "ami-0d6621c01e8c2de2c"
+#   instance_type =   "t2.micro"
+#   key_name      =   "${aws_key_pair.us-west-2-key.key_name}"
+#   security_groups = ["${aws_security_group.allow.name}"]
+#   user_data     =    "${file("httpd.sh")}"
+#   tags = {
+#     Name = "Website"
+#   }
+# }
 
-resource "aws_route53_record" "www" {
-   zone_id = "Z02097382TQD9L135RBW5"
-    name    = "www.turkmendevops.com"
-   type    = "A"
-  ttl     = "5"
-  records = ["${aws_instance.instance.public_ip}"]
-}
-
-
-resource "aws_security_group" "allow" {
-  name        = "allow_tls_azymberdi"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# resource "aws_route53_record" "www" {
+#    zone_id = "Z02097382TQD9L135RBW5"
+#     name    = "www.turkmendevops.com"
+#    type    = "A"
+#   ttl     = "5"
+#   records = ["${aws_instance.instance.public_ip}"]
+# }
 
 
+# resource "aws_security_group" "allow" {
+#   name        = "allow_tls_azymberdi"
+#   description = "Allow TLS inbound traffic"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-#This worked 
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
+
+# #This worked 
