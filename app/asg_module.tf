356f886169a4e67409b00aee6bf77a1269607b3f
azymberdi <azymberdi@gmail.com>
2020-05-06 08:52:41 +0000

    override canceled

--- app/asg_module.tf
+++ app/asg_module.tf
@@ -1,20 +1,6 @@
-module "app1" { 
-source = "./module" 
-region = "us-east-1"
-} 
+# module "app1" { 
+# source = "./module" 
+# region = "us-east-1"
+# } 
 
-module "wordpress" { 
-source = "./module" 
-region = "sa-east-1"
-} 
-
-module "london" { 
-source = "./module" 
-region = "eu-west-2"
-} 
-
-module "frankfurt" { 
-source = "./module" 
-region = "eu-central-1"
-} 
 
